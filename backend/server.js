7be0f3232a24b83b29b6ccbff447d182f0c2155c
filipjazdzewski <filip.r.jazdzewski@gmail.com>
2023-01-15 22:04:13 +0100

    added bodyparser middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 
