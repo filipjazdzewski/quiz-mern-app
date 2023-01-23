87bfe631094053ac4bb06352de1446532df7aac1
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-23 23:39:21 +0100

    create a basic authSlice with initialState

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+  isLoading: false,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default authSlice.reducer;
