548665f1dce639e31d626c27ba85bc4db6e2c992
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-24 13:32:26 +0100

    fix cors issues

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,30 @@ const express = require('express');
 require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
+
+// CORS
+const whitelist = [process.env.FRONT_URL];
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.log(origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 // Connect to DB
 connectDB();
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
