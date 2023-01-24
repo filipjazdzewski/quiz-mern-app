f1d40feeffd0c1b389b5f46ad00cd42571c20192
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-24 13:36:59 +0100

    add register with api request

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,15 +1,43 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
+import { extractErrorMessage } from '../../utils';
+
+// Get user from localstorage
+const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = {
-  user: null,
+  user: user ? user : null,
   isLoading: false,
 };
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(register.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
 });
 
 export default authSlice.reducer;
