5c15d282691ea94001a63493b0383dfd0f490617
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-24 19:09:30 +0100

    change login and register to logout when logged in

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom';
-import { FaSignInAlt, FaUserPlus, FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaSignInAlt, FaUserPlus, FaSignOutAlt } from 'react-icons/fa';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../features/auth/authSlice';
 
 function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    navigate('/');
+  };
+
   return (
     // z-50 makes the navbar on top of every other component fixing the bugs where it was under
     <nav className='navbar mb-12 shadow-lg bg-base-200 bg-opacity-40 backdrop-filter backdrop-blur-lg border-b border-base-300 sticky top-0 z-50 text-neutral-content'>
@@ -41,16 +52,26 @@ function Navbar() {
       <div className='navbar-end'>
         <div className='hidden sm:block'>
           <ul className='flex justify-end px-2 mx-2 gap-2'>
-            <li>
-              <Link to='/login' className='btn btn-ghost btn-sm'>
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to='/register' className='btn btn-primary btn-sm'>
-                Register
-              </Link>
-            </li>
+            {user ? (
+              <li>
+                <button onClick={onLogout} className='btn btn-primary btn-sm'>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link to='/login' className='btn btn-ghost btn-sm'>
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/register' className='btn btn-primary btn-sm'>
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
         <div className='dropdown dropdown-bottom dropdown-end sm:hidden'>
@@ -61,16 +82,28 @@ function Navbar() {
             tabIndex={0}
             className='dropdown-content menu shadow-lg bg-base-200 bg-opacity-40 backdrop-filter backdrop-blur-lg rounded-box w-52 mt-4'
           >
-            <li className='hover-bordered'>
-              <Link to='/login'>
-                <FaSignInAlt /> Login
-              </Link>
-            </li>
-            <li className='hover-bordered'>
-              <Link to='/register'>
-                <FaUserPlus /> Register
-              </Link>
-            </li>
+            {user ? (
+              <>
+                <li className='hover-bordered'>
+                  <button onClick={onLogout}>
+                    <FaSignOutAlt /> Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className='hover-bordered'>
+                  <Link to='/login'>
+                    <FaSignInAlt /> Login
+                  </Link>
+                </li>
+                <li className='hover-bordered'>
+                  <Link to='/register'>
+                    <FaUserPlus /> Register
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
