6238584ef95fa0de615f615592860149b7aad7fb
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-26 16:04:56 +0100

    create a quizList with all the quizzes

---
+++ frontend/src/components/quiz/QuizList.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getQuizzes } from '../../features/quiz/quizSlice';
+import QuizItem from './QuizItem';
+import Spinner from '../../layout/Spinner';
+
+function QuizList() {
+  const dispatch = useDispatch();
+
+  const { quizzes, isLoading } = useSelector((state) => state.quiz);
+
+  useEffect(() => {
+    dispatch(getQuizzes());
+  }, [dispatch]);
+
+  if (isLoading) {
+    <Spinner />;
+  }
+
+  return (
+    <div className='grid grid-cols-1 gap-8 lg:grid-cols-3 sm:grid-cols-2'>
+      {quizzes.map((quiz) => (
+        <QuizItem key={quiz._id} quiz={quiz} />
+      ))}
+    </div>
+  );
+}
+
+export default QuizList;
