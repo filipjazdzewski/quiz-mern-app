42478dbbbb1c2fc2855d677cd56b681849d047a6
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-26 22:03:42 +0100

    add updateUser to res.json

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -104,7 +104,9 @@ const updateQuiz = asyncHandler(async (req, res) => {
     $set: req.body,
   });
 
-  res.status(200).json('Quiz updated');
+  const updatedQuiz = await Quiz.findById(req.params.id);
+
+  res.status(200).json(updatedQuiz);
 });
 
 module.exports = {
