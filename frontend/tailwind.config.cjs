c6f4657ac124166bd2f1428b9a1346fb4b04f7ff
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-27 11:01:13 +0100

    add diasyUi configuration

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,6 +6,12 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
     themes: ['dracula'],
   },
 };
