7959b6f7cb3ff284dc1577c63844f57eb52cfb97
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-01-27 18:07:30 +0100

    add route quiz/play/:id and a footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,34 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
 import Navbar from './layout/Navbar';
+import Footer from './layout/Footer';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import QuizCreator from './pages/QuizCreator';
 import QuizCreatorEdit from './pages/QuizCreatorEdit';
+import QuizPlay from './pages/QuizPlay';
 
 function App() {
   return (
     <>
       <Router>
-        <div>
+        <div className='flex flex-col justify-between h-screen'>
           <Navbar />
+
           <main className='container sm:max-w-screen-lg mx-auto px-3 pb-12'>
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/register' element={<Register />} />
               <Route path='/login' element={<Login />} />
+              <Route
+                path='/quiz/play/:id'
+                element={
+                  <PrivateRoute>
+                    <QuizPlay />
+                  </PrivateRoute>
+                }
+              />
               <Route
                 path='/quiz/creator'
                 element={
@@ -38,6 +49,8 @@ function App() {
               />
             </Routes>
           </main>
+
+          <Footer />
         </div>
       </Router>
       <ToastContainer theme='dark' autoClose={3500} />
