7cc910890df14be9c93a2e77783ffe968f66e2b3
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-02-01 00:37:34 +0100

    add QuizStats and QuizSearch

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import QuizSearch from '../components/quiz/QuizSearch';
+import QuizStats from '../components/quiz/QuizStats';
 import QuizList from '../components/quiz/QuizList';
 
 function Home() {
   return (
-    <>
+    <div className='grid grid-cols-1 gap-8'>
+      <QuizStats />
+      <QuizSearch />
       <QuizList />
-    </>
+    </div>
   );
 }
 
