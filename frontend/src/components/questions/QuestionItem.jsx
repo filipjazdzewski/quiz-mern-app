60112f26891a88e7355378d8f566ee6722c6ed2b
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-02-01 19:46:02 +0100

    change question item layout

--- frontend/src/components/questions/QuestionItem.jsx
+++ frontend/src/components/questions/QuestionItem.jsx
@@ -1,51 +1,41 @@
-import { toast } from 'react-toastify';
 import { RiDeleteBin5Line } from 'react-icons/ri';
-import { useDispatch } from 'react-redux';
-import { deleteQuestion } from '../../features/question/questionSlice';
 
-function QuestionItem({ question, quizId }) {
-  const dispatch = useDispatch();
-
-  const handleDelete = () => {
-    dispatch(deleteQuestion(question._id, quizId))
-      .unwrap()
-      .then(toast.success(`Successfully deleted the question`))
-      .catch(toast.error);
-  };
-
+function QuestionItem({ question, handleDeleteQuestion }) {
   return (
     <>
       <div className='card bg-base-200 shadow-xl'>
         <div className='card-body p-6 pt-2'>
+          <div className='flex gap-2'>
+            <div className='badge badge-secondary badge-outline'>
+              {question.typeOfQuestion}
+            </div>
+            <div className='badge badge-secondary badge-outline'>
+              {question?.options.length} options
+            </div>
+          </div>
           <h2 className='card-title'>
             <div>Question: {question.questionTitle}</div>
           </h2>
-          <div className='badge badge-primary badge-outline'>
-            {question?.options.length} options
-          </div>
-          <div className='badge badge-secondary badge-outline'>
-            {question.typeOfQuestion}
-          </div>
           <div className='card-actions justify-end'>
-            <button onClick={handleDelete} className='btn btn-primary btn-sm'>
+            <button
+              onClick={() => handleDeleteQuestion(question._id)}
+              className='btn btn-primary btn-sm'
+            >
               <RiDeleteBin5Line />
             </button>
           </div>
         </div>
       </div>
-      <div>
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
         {question.options.map((option, index) => (
-          <div
-            className='chat chat-start'
-            key={`${option.optionTitle}${index}`}
-          >
+          <div key={`${option.optionTitle}${index}`}>
             {option.isCorrect ? (
-              <div className='chat-bubble chat-bubble-success'>
-                {option.optionTitle}
+              <div className='card card-compact bg-primary'>
+                <h1 className='card-body'>{option.optionTitle}</h1>
               </div>
             ) : (
-              <div className='chat-bubble chat-bubble-error'>
-                {option.optionTitle}
+              <div className='card card-compact bg-neutral'>
+                <h1 className='card-body'>{option.optionTitle}</h1>
               </div>
             )}
           </div>
