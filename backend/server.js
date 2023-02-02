2db3a6edc302bdb41bbcc08d1453bbfa218507bd
Filip Jażdżewski <filip.r.jazdzewski@gmail.com>
2023-02-02 12:46:32 +0100

    add socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,30 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const cors = require('cors');
-
 const PORT = process.env.PORT || 5000;
 
-// CORS
-const whitelist = [process.env.FRONT_URL];
-const corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.log(origin);
-      callback(new Error('Not allowed by CORS'));
-    }
+const app = express();
+
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONT_URL,
   },
-};
+});
 
 // Connect to DB
 connectDB();
 
-const app = express();
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
 // **** ROUTES ****
 app.use('/api/users', require('./routes/userRoutes'));
@@ -36,6 +32,18 @@ app.use('/api/quizzes', require('./routes/quizRoutes'));
 app.use('/api/questions', require('./routes/questionRoutes'));
 // **** ROUTES ****
 
-app.use(errorHandler);
+// **** SOCKET ****
+io.on('connection', (socket) => {
+  // console.log(`User Connected: ${socket.id}`);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  socket.on('click_like_button', (data) => {
+    socket.broadcast.emit('receive_like_data', data);
+  });
+
+  socket.on('send_questions_length', (data) => {
+    console.log(data);
+    socket.broadcast.emit('receive_questions_length', data);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
